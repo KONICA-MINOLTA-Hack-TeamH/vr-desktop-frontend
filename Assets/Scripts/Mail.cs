b7f85d529cdb8d4e078df56db3bccf7e47c1a78a
karanokuri <karanokuri@gmail.com>
2017-09-17 06:58:25 +0900

    Mailクラスの作成

---
+++ Assets/Scripts/Mail.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Mail
+{
+    public string subject;
+    public string date;
+    public string from;
+    public string to;
+    public string text;
+
+    public static Mail FromJSON(string json)
+    {
+        return JsonUtility.FromJson<Mail>(json);
+    }
+
+    public override string ToString()
+    {
+        string text = "";
+        text += "Title: " + subject + "\n";
+        text += "From:  " + from + "\n";
+        text += "To:    " + to + "\n";
+        text += "Body:\n" + text + "\n";
+        return text;
+    }
+}
