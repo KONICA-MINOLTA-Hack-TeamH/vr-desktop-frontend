c1746c0f8ebf124383995ba60a670ea2a0c15372
mizunekoko <sorata0516@gmail.com>
2017-09-17 01:29:39 +0900

    全部コミット

---
+++ Assets/Scripts/MailViewer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MailViewer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+		
+	public void SetMail(string type, string s){
+		s =  type + " : " + s + "/n";
+		this.GetComponent<TextMesh> ().text = s;
+	}
+	public void Visible(bool t){
+		this.GetComponent<MeshRenderer>().enabled = t;
+	}
+}
---
+++ Assets/Scripts/SkyboxChanger.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkyboxChanger : MonoBehaviour {
+	public Material[] skybox;
+	private int count;
+	private Material m;
+	private int color;
+	// Use this for initializationS
+	void Start () {
+		count = 0;
+		color = 0;
+		m = new Material(skybox[color]);
+		this.GetComponent<Skybox>().material = m; 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		count++;
+		if (count >= 300) {
+			color++;
+			if (color >= 3) {
+				color = 0;
+			}
+			m = new Material (skybox [color]);
+			this.GetComponent<Skybox> ().material = m; 
+			count = 0;
+		}
+	}
+}
