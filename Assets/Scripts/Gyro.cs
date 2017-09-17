1738d06b3f444abe364a4123436544435e0babc8
mizunekoko <sorata0516@gmail.com>
2017-09-17 11:55:55 +0900

    change gyro script

---
+++ Assets/Scripts/Gyro.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gyro : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+		Input.gyro.enabled = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Quaternion gattitude = Input.gyro.attitude;
+		gattitude.x *= -1;
+		gattitude.y *= -1;
+		transform.localRotation = Quaternion.Euler(90, 0, 0) * gattitude;
+	}
+}
